2ef1440498c131e38ac898d57408457129147244
BCH-58 <gfhjigc@gmail.com>
2026-08-25 14:05:59 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -485,8 +485,9 @@ export default function App() {
         )}
       </main>
 
-      <div className="flex justify-center pb-4">
-        <img src={smzLogo} alt="SMZ" style={{ height: 22, opacity: 0.35, objectFit: 'contain' }} />
+      <div className="flex items-center justify-center gap-1.5 pb-4" style={{ opacity: 0.35 }}>
+        <span style={{ fontSize: 10.5, color: C.inkMuted, letterSpacing: 0.5 }}>Developed by</span>
+        <img src={smzLogo} alt="SMZ" style={{ height: 18, objectFit: 'contain' }} />
       </div>
       </div>
     </div>
